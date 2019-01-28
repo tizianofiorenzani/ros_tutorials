69af2f014812afcb2e089ab862a444286c63dc50
tiziano <tiziano.fiorenzani@gmail.com>
2019-01-28 12:34:15 -0600

    pub sub

--- test_pub_sub/src/test_publisher.py
+++ test_pub_sub/src/test_publisher.py
@@ -3,32 +3,27 @@
 import rospy
 from std_msgs.msg import String
 
-#-- String has only one field: data
-
 def publisher():
 
-    #--- queue_size argument limits the amount of queued messages if any
-    #--- subscriber is not receiving them fast enough
     pub     = rospy.Publisher('string_publish', String, queue_size=10)
-    rate    = rospy.Rate(1) #-- publishing rate in Hz
-    msg_to_publish = String()
 
-    print "Initial object to be published"
-    print msg_to_publish
-    print "****************"
+    rate    = rospy.Rate(1)
+
+    msg_to_publish  = String()
+
     counter = 0
 
-    #--- While loop until interrupted
     while not rospy.is_shutdown():
         string_to_publish = "Publishing %d"%counter
         counter += 1
 
-        #--- Assign the string to the data field
         msg_to_publish.data = string_to_publish
-        rospy.loginfo(string_to_publish)
         pub.publish(msg_to_publish)
 
+        rospy.loginfo(string_to_publish)
+
         rate.sleep()
+
 
 if __name__ == "__main__":
     rospy.init_node("simple_publisher")
--- test_pub_sub/src/test_subscriber.py
+++ test_pub_sub/src/test_subscriber.py
@@ -3,17 +3,13 @@
 import rospy
 from std_msgs.msg import String
 
-#-- String has only one field: data
-
 def subscriber():
-    #--- queue_size argument limits the amount of queued messages if any
-    #--- subscriber is not receiving them fast enough
-    sub     = rospy.Subscriber('string_publish', String, callback_function)
+    sub = rospy.Subscriber('string_publish_2', String, callback_function)
+
     rospy.spin()
 
 def callback_function(message):
     rospy.loginfo("I received: %s"%message.data)
-
 
 if __name__ == "__main__":
     rospy.init_node("simple_subscriber")

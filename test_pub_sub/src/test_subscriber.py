b40554d4f2556d9e07f67f5614aae1ca852d87c8
tiziano <tiziano.fiorenzani@gmail.com>
2019-02-01 13:55:07 -0600

    subscriber

--- test_pub_sub/src/test_subscriber.py
+++ test_pub_sub/src/test_subscriber.py
@@ -4,7 +4,7 @@ import rospy
 from std_msgs.msg import String
 
 def subscriber():
-    sub = rospy.Subscriber('string_publish_2', String, callback_function)
+    sub = rospy.Subscriber('string_publish', String, callback_function)
 
     rospy.spin()
 

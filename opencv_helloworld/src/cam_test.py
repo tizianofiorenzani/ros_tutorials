339226fe7f5a20562103b4bc3e9d6b0802dcd869
Tiziano Fiorenzani <tiziano.fiorenzani@gmail.com>
2019-09-18 18:25:37 -0500

    testing opencv

---
+++ opencv_helloworld/src/cam_test.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+#- ON THE RASPI: roslaunch raspicam_node camerav2_1280x960.launch enable_raw:=true
+#
+from __future__ import print_function
+
+#import roslib
+#roslib.load_manifest('my_package')
+import sys
+import rospy
+import cv2
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+class image_converter:
+
+  def __init__(self):
+    self.image_pub = rospy.Publisher("image_topic",Image,queue_size=1)
+
+    self.bridge = CvBridge()
+    self.image_sub = rospy.Subscriber("/raspicam_node/image",Image,self.callback)
+
+  def callback(self,data):
+    try:
+      cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+    except CvBridgeError as e:
+      print(e)
+
+    (rows,cols,channels) = cv_image.shape
+    if cols > 60 and rows > 60 :
+      cv2.circle(cv_image, (500,500), 200, 255)
+      text = "HELLO WORLD"
+      cv2.putText(cv_image, text, (100, 150), cv2.FONT_HERSHEY_SIMPLEX, 5, [0,0,200], 5)
+
+    cv2.imshow("Image window", cv_image)
+    cv2.waitKey(3)
+
+    try:
+      self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
+    except CvBridgeError as e:
+      print(e)
+
+def main(args):
+  ic = image_converter()
+  rospy.init_node('image_converter', anonymous=True)
+  try:
+    rospy.spin()
+  except KeyboardInterrupt:
+    print("Shutting down")
+  cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main(sys.argv)

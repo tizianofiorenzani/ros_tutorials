174ea21dc6d112ca9c2fae8b2b63269a1bc15ae1
Tiziano Fiorenzani <tiziano.fiorenzani@gmail.com>
2019-09-22 10:47:41 -0500

    test input camera

---
+++ opencv_helloworld/cv_test/test_detect_blob_from_camera.py
@@ -0,0 +1,23 @@
+import cv2
+from blob_detector import *
+
+blue_min = (77,40,0)
+blue_max = (101, 255, 255) 
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    
+    keypoints = blob_detect(frame, blue_min, blue_max, 15, blob_params=None)
+    draw_keypoints(frame, keypoints)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
+    
+
